8b56a6ce1757b0bad0885b373f40d9900584c0c7
Ben Nagle-Rose <ben.nagle-rose@redapplecreative.co.uk>
2024-10-02 19:15:06 +0100

    feat: add user selector to header (no styles)

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,9 +1,11 @@
-import { UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
-import { Layout } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
-import "./Page.css";
-import AvatarButton from "./AvatarButton";
 import { Link } from "react-router-dom";
+import { UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
+import { Layout, Select } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
+
+import { useUser } from "./context/UserContext";
+import AvatarButton from "./AvatarButton";
+import "./Page.css";
 
 interface PageProps {
   title: string;
@@ -12,6 +14,11 @@ interface PageProps {
 
 export default function Page(props: PageProps) {
   const { title, children } = props;
+  const { user, setUser } = useUser();
+
+  const handleUserChange = (value: number) => {
+    setUser(value);
+  };
 
   return (
     <Layout>
@@ -25,7 +32,19 @@ export default function Page(props: PageProps) {
           </Link>
         </div>
         <h1>{title}</h1>
-        <div className="page-header-item right">(Toggle user here)</div>
+        <div className="page-header-item right">
+          {/* Simple Ant design component. Controls the current user which can be accessed by other components */}
+          <Select
+            defaultValue={1}
+            onChange={handleUserChange}
+            options={[
+              { value: 1, label: <span>User 1</span> },
+              { value: 2, label: <span>User 2</span> },
+              { value: 3, label: <span>User 3</span> },
+            ]}
+            value={user}
+          />
+        </div>
       </Header>
       <Content className="page-content">{children}</Content>
     </Layout>
